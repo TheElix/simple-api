61be3b30b1d9cbedcad1f930dd87533ebf687765
Dmytro Huz <ualxdima@gmail.com>
2017-02-02 01:48:52 +0200

    Add method to get base64 response and  http code

--- api/GetBase64.php
+++ api/GetBase64.php
@@ -2,5 +2,40 @@
 
 class GetBase64
 {
+    public $file;
+    protected $httpResponseCode;
+
+    public function __construct( $file )
+    {
+        $this->file = $file;
+    }
+
+    public function getResponse()
+    {
+        if (empty($this->file)){
+            $this->httpResponseCode = 500;
+
+            return json_encode(
+                [
+                    "status" => "Error",
+                    "msg" => "Invalid File"
+                ]
+            );
+        }
+        $this->httpResponseCode = 200;
+
+        return json_encode(
+            [
+                "status" => "Success",
+                "msg" => base64_encode(file_get_contents($this->file))
+            ]
+        );
+
+    }
+
+    public function getResponseCode()
+    {
+        return http_response_code($this->httpResponseCode);
+    }
 
 }

595a692ba0dc7cf9bf573285e0083f6946d9be40
Dmytro Huz <ualxdima@gmail.com>
2017-02-02 18:32:47 +0200

    Create method to get translate 'hello' word to user selected language

--- api/SayHelloInLanguage.php
+++ api/SayHelloInLanguage.php
@@ -1,6 +1,41 @@
 <?php
 
-class SayHelloInLanguage
+class SayHelloInLanguage extends SayHello
 {
+    public $language;
+    private static $apiKey = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+    public function __construct($string,$lang)
+    {
+        parent::__construct($string);
+        $this->language = $lang;
+        $this->getTranslated();
+    }
+
+    public function getTranslated()
+    {
+        $googleQuery = "https://www.googleapis.com/language/translate/v2?q=". urlencode(self::$msg) ."&target=".
+                        urlencode($this->language) ."&key=" . urlencode(self::$apiKey);
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $googleQuery,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_CUSTOMREQUEST => "GET",
+        ));
+
+        if( curl_exec($curl) === false ){
+            $this->httpResponseCode = 500;
+        }
+
+        $response = curl_exec($curl);
+
+        $response = json_decode($response);
+        self::$msg = $response->data->translations[0]->translatedText;
+
+        curl_close($curl);
+
+        $this->httpResponseCode = 200;
+    }
 
 }
